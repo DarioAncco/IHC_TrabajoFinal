9403fc6a36e96b611769c760213e4e6f378da028
Michael Secarlos <mapazacals@unsa.edu.pe>
2020-12-19 22:48:52 -0500

    Se ha aÃadido la funcionalidad del CRUD al modulo de categorias

--- ihc/categorias/urls.py
+++ ihc/categorias/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
 	path("registroCategoria",views.registroCategoria,name="registroCategoria"),
 	path("mostrarCategoria",views.mostrarCategoria,name="mostrarCategoria"),
-	]
+	path("actualizarCategoria/<str:id>",views.actualizarCategoria,name="mostrarCategoria"),
+]
--- ihc/categorias/views.py
+++ ihc/categorias/views.py
@@ -16,9 +16,21 @@ def registroCategoria(request):
         )
         categoria.save();
         print("Se ha creado la nueva categoria");
-        return redirect("/")
+        return redirect("/categorias/mostrarCategoria")
     else:
         return render(request,"categoriasForm.html")
 
 def mostrarCategoria(request):
-    return render(request,"categoriasMostrar.html")
+    categorias = Categoria.objects.all();
+    return render(request,"categoriasMostrar.html" , context={"categorias":categorias})
+
+def actualizarCategoria(request , id):
+    categoria = Categoria.objects.get(pk=id);
+    if(request.method == "POST" or None):
+        CatCod = request.POST["cat_cod"]
+        CatDes = request.POST["cat_des"]
+        categoria.CatCod = CatCod
+        categoria.CatDes = CatDes
+        categoria.save()
+        return redirect("/categorias/mostrarCategoria")
+    return render(request , "categoriasUpdate.html" , context={"categoria":categoria})
